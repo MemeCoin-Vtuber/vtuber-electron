14068f353efa6e49401d833ef8982a5a0ecbffa4
Swapnil Shinde <swapnilshinde9382@gmail.com>
2025-07-07 16:17:28 +0700

    fix dimension issues

--- src/main/window-manager.ts
+++ src/main/window-manager.ts
@@ -59,7 +59,6 @@ export class WindowManager {
       height: 1080,
       show: false,
       transparent: true,
-      fullscreen: true,
       backgroundColor: '#ffffff',
       autoHideMenuBar: true,
       frame: false,
